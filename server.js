b62fcf017b2fa7c3f4d5cddb0f5f26c453d6ab87
bandarupallisaketh1 <sakethchowdary112@gmail.com>
2023-06-26 11:38:37 +0530

    twitter-backend

---
+++ server.js
@@ -0,0 +1,44 @@
+//importing moduels
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const auth = require('./routes/auth');
+const user = require('./routes/user');
+const tweet = require('./routes/tweet');
+
+//initialize express
+const app = express();
+//to receive json data
+app.use(express.json());
+//initialize cors which avails any origin
+app.use(cors({
+    origin: '*'
+}));
+
+//connect mongodb with the help of mongoose
+
+mongoose.connect('mongodb+srv://sakethchowdary112:sakethchowdary112@cluster0.3w09wme.mongodb.net/firstdb?retryWrites=true&w=majority').then(
+    console.log("database is connected")
+);
+
+//static the image files avilable to frontend
+app.use("/images", express.static("images"));
+app.use("/tweetimages", express.static("tweetimages"));
+//auth api's
+app.use('/api/auth', auth);
+//user api's
+app.use('/api/user', user);
+//tweet api's
+app.use('/api/tweet', tweet);
+
+
+
+
+
+
+
+
+
+
+
+app.listen(5000, () => console.log("server is running successfully"));
